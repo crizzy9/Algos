2b45a46bc0fd84d77f898c358a30debbf0a5edbd
crizzy9 <shyampadia@live.com>
2018-09-10 16:05:14 -0700

    improved generateParenthesis to 60.22%

--- leetcode/generate_parentheses.py
+++ leetcode/generate_parentheses.py
@@ -1,27 +1,21 @@
 # https://leetcode.com/problems/generate-parentheses/description/
 class Solution:
-    # Beats 43.77% python3 submissions
+    # Beats 60.22% python3 submissions
     def generateParenthesis(self, n):
         out = []
-        combs = [('(', ['('])]
+        combs = [('(', 1)]
         while combs:
-            curr, curr_stack = combs.pop()
-            if len(curr) + len(curr_stack) == 2*n:
-                while curr_stack:
-                    curr_stack.pop()
-                    curr += ')'
+            curr, openings  = combs.pop()
+            if len(curr) + openings == 2*n:
+                curr += ')'*openings
                 out.append(curr)
             else:
-                new_stack = curr_stack.copy()
-                new_stack.append('(')
-
-                if len(curr_stack) == 0:
-                    combs.append((curr + '(', new_stack))
+                if openings == 0:
+                    combs.append((curr + '(', openings + 1))
                 else:
-                    combs.append((curr + '(', new_stack))
-                    curr_stack.pop()
-                    combs.append((curr + ')', curr_stack))
-        print(len(out))        
+                    combs.append((curr + '(', openings + 1))
+                    combs.append((curr + ')', openings - 1))
+        print(len(out))
         return out
 
 
