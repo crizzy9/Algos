1cac8c4f82eef2ddee551e2844916a53f0c64f2f
crizzy9 <shyampadia@live.com>
2018-10-08 13:55:01 -0700

    resolved errors

--- leetcode/group_anagrams.py
+++ leetcode/group_anagrams.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/group-anagrams/description/
+from collections import Counter
 
 class Solution:
     def groupAnagrams(self, strs):
@@ -10,7 +11,7 @@ class Solution:
         group_sets = []
 
         for s in strs:
-            str_set = set(s)
+            str_set = Counter(s)
             if str_set not in group_sets:
                 group_sets.append(str_set)
                 groups.append([s])
@@ -22,3 +23,4 @@ class Solution:
 
 sol = Solution()
 sol.groupAnagrams(['eat', 'tan', 'ate', 'tea', 'nat', 'bat'])
+sol.groupAnagrams(["hos","boo","nay","deb","wow","bop","bob","brr","hey","rye","eve","elf","pup","bum","iva","lyx","yap","ugh","hem","rod","aha","nam","gap","yea","doc","pen","job","dis","max","oho","jed","lye","ram","pup","qua","ugh","mir","nap","deb","hog","let","gym","bye","lon","aft","eel","sol","jab"])
